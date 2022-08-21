e4d5de2f300c77c6103564d44c96469401e7ed0a
Greta <gretugrau@gmail.com>
2022-08-21 20:42:49 -0300

    creacion_header

---
+++ src/app/components/redes-s/redes-s.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-redes-s',
+  templateUrl: './redes-s.component.html',
+  styleUrls: ['./redes-s.component.css']
+})
+export class RedesSComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
